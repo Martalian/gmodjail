78fa3124fa52284ff8307ab119efd6f0ea0ec8f1
ryanskidmore1997 <ryanskidmore1997@9df301e6-f556-11de-aa3c-59de0406b4f5>
2009-12-30 16:55:15 +0000

    Added Combo fists + Made Prisoner have them :)

--- GmodJail/gamemode/init.lua
+++ GmodJail/gamemode/init.lua
@@ -7,6 +7,21 @@ AddCSLuaFile( "cl_hud.lua" )
 
 include( 'shared.lua' )
 
+--Combo Fists,
+
+resource.AddFile("models/weapons/w_fists_t.mdl")
+resource.AddFile("models/weapons/w_fists_t.vvd")
+resource.AddFile("models/weapons/w_fists_t.phy")
+resource.AddFile("models/weapons/w_fists_t.sw.vtx")
+resource.AddFile("models/weapons/w_fists_t.dx90.vtx")
+resource.AddFile("models/weapons/w_fists_t.dx80.vtx")
+resource.AddFile("models/weapons/v_punch.mdl")
+resource.AddFile("models/weapons/v_punch.vvd")
+resource.AddFile("models/weapons/v_punch.sw.vtx")
+resource.AddFile("models/weapons/v_punch.dx90.vtx")
+resource.AddFile("models/weapons/v_punch.dx80.vtx")
+resource.AddFile("sound/weapons/fists/hl2_slash1.wav")
+resource.AddFile("sound/weapons/fists/hl2_slash2.wav")
 
 // Serverside only stuff goes here
 
@@ -31,7 +46,7 @@ if ply:Team() == TEAM_GUARD then
 elseif ply:Team() == TEAM_PRISONER then
 		ply:StripWeapons()
  
-		 ply:Give( "weapon_crowbar" )
+		 ply:Give( "hl2_combo_fists" )
 
 end
 
